2c3b6c7f5c4c5862c33127608abb3123139c15f9
nicollyADS <rm552579@fiap.com.br>
2023-09-04 04:09:39 -0300

    configurando as rotas 2

--- src/main.jsx
+++ src/main.jsx
@@ -36,6 +36,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
