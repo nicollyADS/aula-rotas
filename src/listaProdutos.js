2794dd5089f085fb003df2e842933a3dffbb630c
nicollyADS <rm552579@fiap.com.br>
2023-09-04 04:35:10 -0300

    criando array de produtos

---
+++ src/listaProdutos.js
@@ -0,0 +1,21 @@
+export const listaProdutos = [
+    {
+        id: 1,
+        nome: 'Teclado',
+        preco: 150
+    },
+
+    {
+        id: 2,
+        nome: 'Mouse',
+        preco: 120
+
+    },
+
+    {
+        id: 3,
+        nome: 'Monitor',
+        preco: 950
+
+    }
+]
